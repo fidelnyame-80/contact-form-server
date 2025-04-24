93438d9bd71ccf004ee3ebaa18aef3586f4047c4
fidelnyame-80 <nyamefidel7@gmail.com>
2025-04-24 21:56:13 +0000

    Update package.js

--- package.js
+++ package.js
@@ -1,13 +1,7 @@
 {
   "name": "contact-form-server",
   "version": "1.0.0",
-  "main": "api/contact.js",
-  "scripts": {
-    "start": "node api/contact.js"
-  },
   "dependencies": {
-    "cors": "^2.8.5",
-    "express": "^4.18.2",
     "node-fetch": "^3.3.1"
   }
 }
